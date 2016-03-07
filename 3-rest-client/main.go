e99841408e7f85206c323a34a24128c372112236
John Spence <44@p7-2.hbtn.io>
2016-03-07 15:28:08 -0800

    Completed go tasks

--- 3-rest-client/main.go
+++ 3-rest-client/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+  "net/http"
+  "fmt"
+  "encoding/json"
+)
+
+func main () {
+
+resp, err := http.Get("http://www.omdbapi.com/?i=tt0372784&plot=short&r=json")
+if err != nil {
+  fmt.Println("Error in file retrieval %v", err)
+	// handle error
+}
+defer resp.Body.Close()
+var m Movie
+
+mydecoder :=json.NewDecoder(resp.Body)
+
+if err := mydecoder.Decode(&m); err!=nil{
+fmt.Errorf("error parsing body %v", err)
+return }
+fmt.Printf("%#v",m.Actors)
+
+
+fmt.Println("status code is", resp.Status)
+}
